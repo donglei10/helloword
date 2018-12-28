fcd96ae856d11995d715cf29edbeb20dba46d848
donglei <Administrator@donglei>
2018-12-28 16:53:19 +0800

    session 共享提交

--- dl_provider/dl-provider-web/src/main/java/com/dl/springcloud/system/LoginController.java
+++ dl_provider/dl-provider-web/src/main/java/com/dl/springcloud/system/LoginController.java
@@ -1,8 +1,12 @@
 package com.dl.springcloud.system;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,18 +19,31 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by Administrator on 2017/10/29.
  */
-@RestController 
-@RefreshScope
+//@RestController 
+//@RefreshScope
+@Controller 
 public class LoginController {
 
 	//@Resource(name="helloServiceImpl")
 	//private HelloService service;
 	 
 	
-	@RequestMapping("/login")
+	@RequestMapping("/")
     public String index(Model model, HttpServletResponse response) {
-        model.addAttribute("name", "simonsfan");
+        
         return "login";
     }
+	
+	@RequestMapping("/login")
+	public String dologin(Model model,HttpServletRequest request,String username,String password){
+		   HttpSession session = request.getSession();
+	        String sessionUsername = (String) session.getAttribute("username");
+	        if (StringUtils.isEmpty(sessionUsername)) {
+	            session.setAttribute("username", sessionUsername);
+	        }
+	        System.out.println("用户：" + username);
+	        System.out.println("访问端口：" + request.getServerPort());
+	        return "/index";
+	}
 
 }
